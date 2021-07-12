892b9e18b8c6ca89564e8d8a0e43c1a8d7c48052
Renan Pereira <renanmol87@gmail.com>
2021-07-12 11:08:56 -0300

    feat: create types

---
+++ src/domain/posts/index.ts
@@ -0,0 +1,72 @@
+type PostAuthor = {
+  id: number
+  name: string
+  createdBy: number
+  updatedBy: number
+  createdAt: string
+  updatedAt: string
+}
+
+type PostCategory = {
+  id: number
+  name: string
+  createdBy: number
+  updatedBy: number
+  createdAt: string
+  updatedAt: string
+}
+
+type PostCreatedBy = {
+  id: number
+  firstname: string
+  lastname: string
+  username: null
+}
+
+type PostCoverFormat = {
+  ext: string
+  url: string
+  hash: string
+  mime: string
+  name: string
+  path?: null
+  size: number
+  width: number
+  height: number
+  provider_metadata: {
+    public_id: string
+    resource_type: string
+  }
+}
+
+export type PostCover = PostCoverFormat & {
+  id: number
+  alternativeText: string
+  caption: string
+  previewUrl: null
+  provider: string
+  createdBy: string
+  updatedBy: string
+  createdAt: string
+  updatedAt: string
+  formats: {
+    thumbnail: PostCoverFormat
+    small: PostCoverFormat
+    medium: PostCoverFormat
+    large: PostCoverFormat
+  }
+}
+
+export type PostData = {
+  id: number
+  title: string
+  content: string
+  slug: string
+  author: PostAuthor
+  category: PostCategory
+  cover: PostCover
+  createdBy: PostCreatedBy
+  updatedBy: PostCreatedBy
+  createdAt: string
+  updatedAt: string
+}
