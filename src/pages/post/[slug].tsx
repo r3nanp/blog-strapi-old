9b9a8753668b4ce45461a2ea80fc04132f240cfd
Renan Pereira <renanmol87@gmail.com>
2021-07-12 21:21:35 -0300

    feat(post): create new page

---
+++ src/pages/post/[slug].tsx
@@ -0,0 +1,73 @@
+import { GetStaticPaths, GetStaticProps } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import { api } from 'services/api'
+import { Header } from '@components/Header'
+
+import { formatDate } from 'utils/format-date'
+import type { PostData } from 'domain/posts'
+
+type PostProps = {
+  post: PostData
+}
+
+export default function Post({ post }: PostProps) {
+  const { title, content, cover, createdAt } = post
+
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+      </Head>
+
+      <Header title="Blog" />
+
+      <main>
+        <Image src={cover.url} width={400} height={400} />
+
+        <div dangerouslySetInnerHTML={{ __html: content }} />
+
+        <p>Post created at: {createdAt}</p>
+      </main>
+    </>
+  )
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const { data } = await api.get<PostData[]>('/posts')
+
+  const paths = data.map(post => {
+    return {
+      params: {
+        slug: String(post.id)
+      }
+    }
+  })
+
+  return {
+    paths,
+    fallback: 'blocking'
+  }
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const { slug } = params
+
+  const { data } = await api.get(`/posts/${slug}`)
+
+  const post = {
+    id: data.id,
+    title: data.title,
+    content: data.content,
+    author: data.author,
+    cover: data.cover,
+    createdAt: formatDate(data.created_at)
+  }
+
+  return {
+    props: {
+      post
+    },
+    revalidate: 60 * 60 * 24
+  }
+}
