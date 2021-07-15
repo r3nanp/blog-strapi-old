dd08504f428cac14124f72568b67a2f4e8ffadfe
Renan Pereira <renanmol87@gmail.com>
2021-07-15 20:40:46 -0300

    feat: add links to header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,29 @@
+import { ReactNode } from 'react'
 import Link from 'next/link'
 import * as S from './styles'
 
 export type HeaderProps = {
   title: string
+  icon?: ReactNode
 }
 
-export function Header({ title }: HeaderProps) {
+export function Header({ title, icon }: HeaderProps) {
   return (
     <S.Container>
-      <Link href="/">
-        <a>{title}</a>
-      </Link>
+      {!!icon && <S.Icon>{icon}</S.Icon>}
+
+      <S.Title>{title}</S.Title>
+      <S.LeftContent>
+        <Link href="/">
+          <a>Blog</a>
+        </Link>
+        <Link href="/about">
+          <a>About</a>
+        </Link>
+        <Link href="/projects">
+          <a>Projects</a>
+        </Link>
+      </S.LeftContent>
     </S.Container>
   )
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,20 +1,58 @@
 import styled, { css } from 'styled-components'
+import media from 'styled-media-query'
 
 export const Container = styled.header`
   ${({ theme }) => css`
     display: flex;
     align-items: center;
-    justify-content: center;
+    justify-content: space-between;
 
     width: 100%;
     height: 4rem;
-    background: ${theme.colors.primary};
-    font-size: ${theme.font.sizes.large};
-    color: ${theme.colors.white};
+    background: ${theme.colors.white};
+    font-size: ${theme.font.sizes.small};
+    color: ${theme.colors.primary};
     padding: ${theme.spacings.medium};
+    border-bottom: 2px solid ${theme.colors.gray.dark};
+
+    ${media.lessThan('small')`
+        height: 2rem;
+      `}
+  `}
+`
+
+export const Title = styled.h1`
+  ${({ theme }) => css`
+    font-size: ${theme.font.sizes.small};
+    line-height: 1.4;
+    letter-spacing: 0.0875rem;
+  `}
+`
+
+export const LeftContent = styled.nav`
+  ${({ theme }) => css`
+    font-size: ${theme.font.sizes.xsmall};
+    font-weight: ${theme.font.light};
+
+    ${media.lessThan('medium')`
+        padding: 0.2rem;
+      `}
 
     a {
-      color: ${theme.colors.white};
+      ${media.lessThan('medium')`
+        display: none;
+      `}
+
+      color: ${theme.colors.primary};
+      margin: ${theme.spacings.small};
+
+      transition: all ${theme.transition.default};
+
+      &:hover {
+        text-decoration: underline;
+      }
     }
   `}
 `
+
+export const Icon = styled.div``
