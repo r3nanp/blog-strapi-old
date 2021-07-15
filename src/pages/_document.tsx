78fbd0455bfad54c3b1cc1c257d2ea3cbe39fa30
Renan Pereira <renanmol87@gmail.com>
2021-07-15 20:08:05 -0300

    chore: add new fonts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -46,7 +46,7 @@ export default class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
-            href="https://fonts.googleapis.com/css2?family=Inter:wght@600;700&family=Roboto:wght@400;500&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap"
             rel="stylesheet"
           />
           <script
